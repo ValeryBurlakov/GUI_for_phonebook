aba6b79553b88f1744c3a903142174b65c2399e9
ValeryBurlakov <wogl@mail.ru>
2023-03-02 03:18:27 +0300

    start of GUI development for phonebook

--- GUI.py
+++ GUI.py
@@ -1,49 +1,112 @@
 import tkinter as tk # импорт библиотеки
 from tkinter import * # импорт всех методов
 from tkinter import messagebox # явное импортирование
+from tkinter import ttk  
+from datetime import datetime
 
+def finish():
+    window.destroy()  # ручное закрытие окна и всего приложения
+    print("Закрытие приложения")
+    messagebox.showinfo('answer-pythonguides', f'Ты закрыл это божественное приложение!!!')
+
+def enter_(event):
+    # messagebox.showinfo('answer-pythonguides', f'Вы ввели enter')
+    main_()
+
+
+def main_():
+    global answer
+    answer = user_input.get()
+    dt = datetime.now()
+    # messagebox.showinfo('Ебучая книга', f'Доброго времени суток! {dt.strftime("%A, %d %B %Y %I:%M%p")}')
+    if answer == '0':
+                # messagebox.showinfo('answer-pythonguides', f'Ты гандон ввёл 0')
+                menu()
+                # out_field = Label(
+                # frame, 
+                # text = f'Ты ввёл: {answer}',
+                # )
+                # out_field.grid(row=5, column=1)
+
+                # out_field2 = Label(
+                # frame, 
+                # text = f'Ты ввёл: {int(answer) + 1}',
+
+                # )
+                # out_field2.grid(row=6, column=1)
+    elif answer == '1':
+            ui.menu()
+    elif answer == '2':
+                messagebox.showinfo('answer-pythonguides', f'Ты гандон ввёл 2')
+    elif answer == '9':
+              print("получай в консоли exit")
+
+def menu():
+    out_field_0 = Label(
+    frame, 
+    text = f'0 - вызов меню',
+    )
+    out_field_0.grid(row=4, column=1) 
+    
+    out_field_1 = Label(
+    frame, 
+    text = f'1 - показать справочник',
+    )
+    out_field_1.grid(row=5, column=1)
+
+    out_field_2 = Label(
+    frame, 
+    text = f'2 - добавить новый контакт',
+    )
+    out_field_2.grid(row=6, column=1)
+
+    # print("\033[32m2\033[0m - добавить новый контакт")
+    # print("\033[32m3\033[0m - удалить контакт")
+    # print("\033[32m4\033[0m - поиск контакта(нужно ввести имя целиком)")
+    # print("\033[32m5\033[0m - продвинутый поиск(по всем данным)")
+    # print("\033[32m6\033[0m - выгрузка данных")
+    # print("\033[32m7\033[0m - изменение данных")
+    # print("\033[32m8\033[0m - Копирование справочника в файл")
+    # print("\033[32m9\033[0m - выход из программы")
 
 
 
 window = Tk()
 window.title("Телефонная книга")
-window.geometry('400x300')
+window.geometry('600x400')
+window.bind('<Return>', enter_)
+window.protocol("WM_DELETE_WINDOW", finish)
 
-def main():
-    input_ = int(user_input.get())
-    if user_input == str(0):
-        messagebox.showinfo('command-pythonguides', f'Вы ввели 0')
-    elif user_input == str(1):
-        messagebox.showinfo('command-pythonguides', f'Вы ввели 1')
 frame = Frame(
-    window, # указывает окно для размещения frame
-    padx = 10, # отступы
-    pady = 10
+window, # указывает окно для размещения виджета frame для контроля расположения элементов
+padx = 10, # отступы
+pady = 10
 )
 frame.pack(expand=True) # позиционируем виджет в окне
 
 # добавили виджет label для поля ввода
 entry_field = Label(
     frame, # заготовка виджета в которой уже настроены отступы по фертикали и горизонтали
-    text = "Введите что-нибудь "
+    text = "Введите ваше имя "
 )
-
 entry_field.grid(row=3, column=1) # grid метод позиционирования виджета в окне
+
+
+
+# виджет ввода
 user_input = Entry(
     frame, # заготовка с отступами
 )
 user_input.grid(row=3, column=2, pady=5)
 
+# кнопка
 
-
-
-button_start = Button(
+button_start = ttk.Button(
     frame,
     text= 'Ввод',
-    command = main
+    command = main_
 )
 button_start.grid(row=5, column=2) # разместили кнопку в ячейке
-
 
 
 
--- test.py
+++ test.py
@@ -1,61 +1,142 @@
+import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 
-def calculate_bmi():
-   kg = int(weight_tf.get())
-   m = int(height_tf.get())/100
-   bmi = kg/(m*m)
-   bmi = round(bmi, 1)
+# app = tk.Tk()
+# app.geometry("200x100")
+
+# def callback(event):
+#     label["text"] = "You pressed Enter"
+
+# app.bind('<Return>', callback)
+
+# label = tk.Label(app, text="")
+# label.pack()
+
+# app.mainloop()
+
+
+# import tkinter as tk
+
+# class app(tk.Frame):
+#     def __init__(self):
+#         self.root = tk.Tk()
+#         self.root.geometry("300x200")
+#         self.label = tk.Label(self.root, text="")
+#         self.label.pack()
+#         self.root.bind('<Return>', self.callback)
+#         self.root.mainloop()
+
+
+#     def callback(self, event):
+#         self.label["text"] = "You pressed {}".format(event.keysym)
+    
+# app()
+
+
+import tkinter as tk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root)
+canvas.pack()
+
+circle = canvas.create_oval((150, 100, 170, 120), fill='yellow')
+vx = 0
+vy = 0
+
+
+def on_key_press(event):
+    global vx, vy
+    if event.keysym == 'Left':
+        vx = -10
+    elif event.keysym == 'Right':
+        vx = 10
+    elif event.keysym == 'Up':
+        vy = -10
+    elif event.keysym == 'Down':
+        vy = 10
+
+
+def on_key_release(event):
+    global vx, vy
+    if event.keysym in ('Left', 'Right'):
+        vx = 0
+    elif event.keysym in ('Up', 'Down'):
+        vy = 0
+
+
+root.bind('<KeyPress>', on_key_press)
+root.bind('<KeyRelease>', on_key_release)
+
+
+def game_loop():
+    canvas.move(circle, vx, vy)
+    root.after(50, game_loop)
+
+
+game_loop()
+
+root.mainloop()
+
+
+
+
+# def calculate_bmi():
+#    kg = int(weight_tf.get())
+#    m = int(height_tf.get())/100
+#    bmi = kg/(m*m)
+#    bmi = round(bmi, 1)
  
-   if bmi < 18.5:
-       messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует недостаточному весу')
-   elif (bmi > 18.5) and (bmi < 24.9):
-       messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует нормальному весу')
-   elif (bmi > 24.9) and (bmi < 29.9):
-       messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует избыточному весу')
-   else:
-       messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует ожирению')  
+#    if bmi < 18.5:
+#        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует недостаточному весу')
+#    elif (bmi > 18.5) and (bmi < 24.9):
+#        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует нормальному весу')
+#    elif (bmi > 24.9) and (bmi < 29.9):
+#        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует избыточному весу')
+#    else:
+#        messagebox.showinfo('bmi-pythonguides', f'ИМТ = {bmi} соответствует ожирению')  
  
-window = Tk()
-window.title('Калькулятор индекса массы тела (ИМТ)')
-window.geometry('400x300')
- 
- 
-frame = Frame(
-   window,
-   padx=10,
-   pady=10
-)
-frame.pack(expand=True)
+# window = Tk()
+# window.title('Калькулятор индекса массы тела (ИМТ)')
+# window.geometry('400x300')
  
  
-height_lb = Label(
-   frame,
-   text="Введите свой рост (в см)  "
-)
-height_lb.grid(row=3, column=1)
+# frame = Frame(
+#    window,
+#    padx=10,
+#    pady=10
+# )
+# frame.pack(expand=True)
  
-weight_lb = Label(
-   frame,
-   text="Введите свой вес (в кг)  ",
-)
-weight_lb.grid(row=4, column=1)
  
-height_tf = Entry(
-   frame,
-)
-height_tf.grid(row=3, column=2, pady=5)
+# height_lb = Label(
+#    frame,
+#    text="Введите свой рост (в см)  "
+# )
+# height_lb.grid(row=3, column=1)
  
-weight_tf = Entry(
-   frame,
-)
-weight_tf.grid(row=4, column=2, pady=5)
+# weight_lb = Label(
+#    frame,
+#    text="Введите свой вес (в кг)  ",
+# )
+# weight_lb.grid(row=4, column=1)
  
-cal_btn = Button(
-   frame,
-   text='Рассчитать ИМТ',
-   command=calculate_bmi
-)
-cal_btn.grid(row=5, column=2)
+# height_tf = Entry(
+#    frame,
+# )
+# height_tf.grid(row=3, column=2, pady=5)
  
-window.mainloop()
+# weight_tf = Entry(
+#    frame,
+# )
+# weight_tf.grid(row=4, column=2, pady=5)
+ 
+# cal_btn = Button(
+#    frame,
+#    text='Рассчитать ИМТ',
+#    command=calculate_bmi
+# )
+# cal_btn.grid(row=5, column=2)
+ 
+# window.mainloop()
